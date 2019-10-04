967e68ece1be567bf62c5db50a802fb83f179eb7
siner <siner308@gmail.com>
2019-10-04 21:56:21 +0900

    add feature gif upload

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
 const Room = require('../schemas/room');
 const Chat = require('../schemas/chat');
@@ -16,7 +19,7 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/room', (req, res) => {
-    res.render('room', { title: 'GIF coxlㅇ방 생성' });
+    res.render('room', { title: 'GIF 채팅방 생성' });
 });
 
 router.post('/room', async (req, res, next) => {
@@ -54,10 +57,11 @@ router.get('/room/:id', async (req, res, next) => {
             req.flash('roomError', '허용 인원이 초과하였습니다.');
             return res.redirect('/');
         }
+        const chats = await Chat.find({ room: room._id }).sort('createdAt');
         return res.render('chat', {
             room,
             title: room.title,
-            chats: [],
+            chats,
             user: req.session.color,
         });
     } catch (error) {
@@ -79,4 +83,55 @@ router.delete('/room/:id', async (req, res, next) => {
         next(error);
     }
 });
+
+router.post('/room/:id/chat', async (req, res, next) => {
+    try {
+        const chat = new Chat({
+            room: req.params.id,
+            user: req.session.color,
+            chat: req.body.chat,
+        });
+        await chat.save();
+        req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+        res.send('ok');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+fs.readdir('uploads', (error) => {
+    if (error) {
+        console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+        fs.mkdirSync('uploads');
+    }
+});
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, cb) {
+            cb(null, 'uploads/');
+        },
+        filename(req, file, cb) {
+            const ext = path.extname(file.originalname);
+            cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+router.post('/room/:id/gif', upload.single('gif'), async (req, res, next) => {
+    try {
+        const chat = new Chat({
+            room: req.params.id,
+            user: req.session.color,
+            gif: req.file.filename,
+        });
+        await chat.save();
+        req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+        res.send('ok');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
 module.exports = router;
